5702acc0921ce3668e6fd09f8eb68899c5537ff2
Roz Bryant Dupiton <rozbryant@gmail.com>
2016-11-08 15:49:21 -0600

    Submission

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,8 +4,26 @@ class ActorsController < ApplicationController
     @actor = Actor.all
   end
 
-def show
-  @actor = Actor.find(params[:id])
-end
+  def show
+    @actor = Actor.find(params[:id])
+  end
+
+  def new_form
+    render("/actors/new_form.html.erb")
+  end
+
+  def create_row
+    @actor = Actor.new
+    @actor.name = params[:name]
+    @actor.dob = params[:dob]
+    @actor.bio = params[:bio]
+    @actor.image_url = params[:image_url]
+
+    @actor.save
+
+    render("show")
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get('/',  { :controller => 'movies', :action => 'index'})
 
   #Actors routes
+  #CREATE
+  get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
+  get('/create_actor', { :controller => 'actors', :action => 'create_row' })
+
   #READ
   get('/actors',  { :controller => 'actors', :action => 'index'})
   get('actors/:id', { :controller => 'actors', :action => 'show'})
